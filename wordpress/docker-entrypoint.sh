910568d4b1d30a672f0d85db95408ef2eeeb305e
CustomBread <seb9000@gmail.com>
2016-11-20 18:05:52 +0100

    wip

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -15,14 +15,11 @@ EOF
 chown www-data:www-data .htaccess
 
 # set apache as owner/group
-if [ "$FIX_OWNERSHIP" != "" ]; then
-        chown -R apache:apache /app
-fi
+chown -R apache:apache /app
 
 echo "[i] Starting daemon..."
-# run apache httpd daemon
-#httpd -D FOREGROUND
 exec httpd -DFOREGROUND
+
 #while true; do sleep 1000; done
 
 
