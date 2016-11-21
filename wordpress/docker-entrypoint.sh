303f02838abd0424d9bc40252a63d247bdc7c433
CustomBread <CustomBread@users.noreply.github.com>
2016-11-21 21:31:12 +0100

    update wordpress (fonctionnel)

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-#cat > /var/www/localhost/htdocs/wordpress/.htaccess <<-'EOF'
-#  # BEGIN WordPress
-#  <IfModule mod_rewrite.c>
-#  RewriteEngine On
-#  RewriteBase /
-#  RewriteRule ^index\.php$ - [L]
-#  RewriteCond %{REQUEST_FILENAME} !-f
-#  RewriteCond %{REQUEST_FILENAME} !-d
-#  RewriteRule . /index.php [L]
-#  </IfModule>
-#  # END WordPress
-#order allow,deny
-#allow from all
-#EOF
-
-cat > /var/www/localhost/htdocs/wordpress/.htaccess <<-'EOF'
+cat > /var/www/localhost/htdocs/.htaccess <<-'EOF'
 # BEGIN WordPress
 RewriteEngine On
 RewriteBase /
@@ -23,14 +8,9 @@ RewriteRule ^index\.php$ - [L]
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule . /index.php [L]
+# END WordPress
 order allow,deny
 allow from all
-# END WordPress
-EOF
-
-cat > /var/www/localhost/htdocs/.htaccess <<-'EOF'
-order allow,deny
-deny from all
 EOF
 
 chown -R apache:apache /var/www/localhost/htdocs
