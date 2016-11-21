237b3e87c914b6ab5efa22e6f750ab0383840a3e
CustomBread <CustomBread@users.noreply.github.com>
2016-11-21 20:27:45 +0100

    update wordpress

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 
-cat > .htaccess <<-'EOF'
-  # BEGIN WordPress
-  <IfModule mod_rewrite.c>
-  RewriteEngine On
-  RewriteBase /
-  RewriteRule ^index\.php$ - [L]
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteCond %{REQUEST_FILENAME} !-d
-  RewriteRule . /index.php [L]
-  </IfModule>
-  # END WordPress
+#cat > /var/www/localhost/htdocs/wordpress/.htaccess <<-'EOF'
+#  # BEGIN WordPress
+#  <IfModule mod_rewrite.c>
+#  RewriteEngine On
+#  RewriteBase /
+#  RewriteRule ^index\.php$ - [L]
+#  RewriteCond %{REQUEST_FILENAME} !-f
+#  RewriteCond %{REQUEST_FILENAME} !-d
+#  RewriteRule . /index.php [L]
+#  </IfModule>
+#  # END WordPress
+#order allow,deny
+#allow from all
+#EOF
+
+cat > /var/www/localhost/htdocs/wordpress/.htaccess <<-'EOF'
+# BEGIN WordPress
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+order allow,deny
+allow from all
+# END WordPress
 EOF
-chown apache:apache .htaccess
-# set apache as owner/group
-chown -R apache:apache /app
+
+cat > /var/www/localhost/htdocs/.htaccess <<-'EOF'
+order allow,deny
+deny from all
+EOF
+
+chown -R apache:apache /var/www/localhost/htdocs
 
 echo "[i] Starting daemon..."
 exec httpd -DFOREGROUND
-
-
